c9addba44b2ee881887826a00b0cf73437739c24
Catheryne Small <catheryn.small@gmail.com>
2020-06-02 02:51:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 class CashRegister
   attr_accessor :total, :discount
   
-  def initialize(total = 0.00, discount = 20.00)
+  def initialize(discount = 0)
     @total = total
     @discount = discount
   end
